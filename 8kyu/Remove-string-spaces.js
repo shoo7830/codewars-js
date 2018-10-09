0577fdea7286be584579139a16994bba61fc4c3b
Aria Kim <shoo7830@gmail.com>
2018-10-10 08:59:35 +0900

    Update Remove-string-spaces.js

--- 8kyu/Remove-string-spaces.js
+++ 8kyu/Remove-string-spaces.js
@@ -1,1 +1,16 @@
 //Simple, remove the spaces from the string, then return the resultant string.
+
+// my solution
+// 정규표현식 참고, 필요없이 변수선언.
+function noSpace(x){
+  let removeSpace = x.replace(/(\s*)/g,'');
+  return removeSpace;
+}
+
+// best practice
+function noSpace(x){
+  return x.replace(/\s/g, '');
+}
+
+// ES6
+const noSpace = (str) => str.replace(/\s*/g, '');

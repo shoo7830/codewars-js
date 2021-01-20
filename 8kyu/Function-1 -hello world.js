6cc46089b308a1297c7e5e432a1a693685634f17
Aria Kim <shoo7830@gmail.com>
2021-01-20 16:42:31 +0900

    Create Function-1 -hello world.js

---
+++ 8kyu/Function-1 -hello world.js
@@ -0,0 +1,10 @@
+// Make a simple function called greet that returns the most-famous "hello world!".
+// 개 쉬움
+
+// 내 답변 === 베스트 답변
+function greet() {
+  return "hello world!";
+}
+
+// ES6+
+const greet = () => "hello world!";
